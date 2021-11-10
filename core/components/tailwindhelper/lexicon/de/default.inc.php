9e18f56d640c464539955337c96dc7737ac0378d
Thomas Jakobi <buero@treehillstudio.de>
2021-11-10 21:32:08 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: MODX Extras/MODX TailwindHelper/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-tailwindhelper-standard/de/

--- core/components/tailwindhelper/lexicon/de/default.inc.php
+++ core/components/tailwindhelper/lexicon/de/default.inc.php
@@ -7,10 +7,10 @@
  */
 $_lang['tailwindhelper'] = 'TailwindHelper';
 $_lang['tailwindhelper.menu'] = 'TailwindHelper';
-$_lang['tailwindhelper.menu_desc'] = 'Update Tailwind Safelist';
-$_lang['tailwindhelper.scan_chunks'] = 'Scanned chunks content for CSS classes:';
-$_lang['tailwindhelper.scan_found'] = 'Found total [[+count]] CSS classes.';
-$_lang['tailwindhelper.scan_resources'] = 'Scanned resources content for CSS classes:';
-$_lang['tailwindhelper.scan_result'] = 'The class list is saved in "[[+path]]".';
-$_lang['tailwindhelper.scan_templates'] = 'Scanned templates content for CSS classes:';
-$_lang['tailwindhelper.scan_tvs'] = 'Scanned template variables content for CSS classes:';
+$_lang['tailwindhelper.menu_desc'] = 'Tailwind Safelist aktualisieren';
+$_lang['tailwindhelper.scan_chunks'] = 'Chunk-Inhalt gescannt nach CSS-Klassen:';
+$_lang['tailwindhelper.scan_found'] = 'Insgesamt [[+count]] CSS-Klassen gefunden.';
+$_lang['tailwindhelper.scan_resources'] = 'Ressourcen-Inhalt gescannt nach CSS-Klassen:';
+$_lang['tailwindhelper.scan_result'] = 'Die Klassenliste wird in "[[+ path]]" gespeichert.';
+$_lang['tailwindhelper.scan_templates'] = 'Template-Inhalt gescannt nach CSS-Klassen:';
+$_lang['tailwindhelper.scan_tvs'] = 'Template-Variablen-Inhalt gescannt nach CSS-Klassen:';
