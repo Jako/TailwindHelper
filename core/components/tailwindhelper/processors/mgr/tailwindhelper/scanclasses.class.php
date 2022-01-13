b5d110e2af13e35c9716a398997d05f671556f48
Thomas Jakobi <thomas.jakobi@partout.info>
2022-01-13 20:02:55 +0100

    Normalize code

--- core/components/tailwindhelper/processors/mgr/tailwindhelper/scanclasses.class.php
+++ core/components/tailwindhelper/processors/mgr/tailwindhelper/scanclasses.class.php
@@ -28,8 +28,8 @@ class TailwindHelperScanClassesProcessor extends Processor
             $classes = array_merge($classes, $this->tailwindhelper->getDefaultClasses($chunkContent));
             $classes = array_merge($classes, $this->tailwindhelper->getAlpineClasses($chunkContent));
         }
-        $this->modx->log(modX::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_chunks'));
-        $this->modx->log(modX::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_found', ['count' => count($classes)]));
+        $this->modx->log(xPDO::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_chunks'));
+        $this->modx->log(xPDO::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_found', ['count' => count($classes)]));
 
         /** @var \modTemplate[] $templates */
         $templates = $this->modx->getIterator('modTemplate');
@@ -39,8 +39,8 @@ class TailwindHelperScanClassesProcessor extends Processor
             $classes = array_merge($classes, $this->tailwindhelper->getDefaultClasses($templateContent));
             $classes = array_merge($classes, $this->tailwindhelper->getAlpineClasses($templateContent));
         }
-        $this->modx->log(modX::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_templates'));
-        $this->modx->log(modX::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_found', ['count' => count($classes)]));
+        $this->modx->log(xPDO::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_templates'));
+        $this->modx->log(xPDO::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_found', ['count' => count($classes)]));
 
         /** @var \modResource[] $resources */
         $resources = $this->modx->getIterator('modResource');
@@ -50,8 +50,8 @@ class TailwindHelperScanClassesProcessor extends Processor
             $classes = array_merge($classes, $this->tailwindhelper->getDefaultClasses($resourceContent));
             $classes = array_merge($classes, $this->tailwindhelper->getAlpineClasses($resourceContent));
         }
-        $this->modx->log(modX::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_resources'));
-        $this->modx->log(modX::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_found', ['count' => count($classes)]));
+        $this->modx->log(xPDO::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_resources'));
+        $this->modx->log(xPDO::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_found', ['count' => count($classes)]));
 
         /** @var \modTemplateVarResource[] $templateVars */
         $templateVars = $this->modx->getIterator('modTemplateVarResource');
@@ -61,8 +61,8 @@ class TailwindHelperScanClassesProcessor extends Processor
             $classes = array_merge($classes, $this->tailwindhelper->getDefaultClasses($templateVarContent));
             $classes = array_merge($classes, $this->tailwindhelper->getAlpineClasses($templateVarContent));
         }
-        $this->modx->log(modX::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_tvs'));
-        $this->modx->log(modX::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_found', ['count' => count($classes)]));
+        $this->modx->log(xPDO::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_tvs'));
+        $this->modx->log(xPDO::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_found', ['count' => count($classes)]));
 
         $classes = array_unique(array_filter($classes));
         sort($classes);
@@ -75,7 +75,7 @@ class TailwindHelperScanClassesProcessor extends Processor
         }
         $this->modx->cacheManager->writeFile($path . 'safelist.json', json_encode($classes, JSON_PRETTY_PRINT));
 
-        $this->modx->log(modX::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_result', ['path' => $path]));
+        $this->modx->log(xPDO::LOG_LEVEL_INFO, $this->modx->lexicon('tailwindhelper.scan_result', ['path' => $path]));
 
         sleep(1);
 
