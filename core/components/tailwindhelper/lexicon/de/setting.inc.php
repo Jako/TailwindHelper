a725ea5072a394849f81b1664da5a7d70ba980a9
Thomas Jakobi <buero@treehillstudio.de>
2025-02-27 13:13:09 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: MODX Extras/System Settings
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/tailwindhelper/system-settings/de/

--- core/components/tailwindhelper/lexicon/de/setting.inc.php
+++ core/components/tailwindhelper/lexicon/de/setting.inc.php
@@ -8,7 +8,7 @@
 $_lang['setting_tailwindhelper.debug'] = 'Debug';
 $_lang['setting_tailwindhelper.debug_desc'] = 'Debug-Informationen im MODX Fehlerprotokoll ausgeben.';
 $_lang['setting_tailwindhelper.filepaths'] = 'Dateipfade';
-$_lang['setting_tailwindhelper.filepaths_desc'] = 'Durch Kommata getrennte Liste von Dateipfaden, die rekursiv nach Dateien mit dem Mimetype text/plain oder text/html durchsucht werden. Der Inhalt dieser Dateien wird nach CSS Klassen für die  safelist.json durchsucht.';
+$_lang['setting_tailwindhelper.filepaths_desc'] = 'Durch Kommata getrennte Liste von Dateipfaden, die rekursiv nach Dateien mit dem Mimetype text/plain oder text/html durchsucht werden. Der Inhalt dieser Dateien wird nach CSS Klassen für die safelist.json durchsucht.';
 $_lang['setting_tailwindhelper.removeModxTags'] = 'MODX-Tags entfernen';
 $_lang['setting_tailwindhelper.removeModxTags_desc'] = 'Verbleibende MODX-Tags in der safelist.json entfernen.';
 $_lang['setting_tailwindhelper.safelistFolder'] = 'Safelist Ordner';
